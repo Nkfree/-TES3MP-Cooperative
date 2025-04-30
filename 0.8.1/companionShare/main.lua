9f00c9f2cd5107c65c3bca114efb0958df78fc1b
Nkfree <BlackVaclav@gmail.com>
2025-04-30 17:15:31 +0200

    Update to 1.1
    1.1 - removed timer for checking player standing still, newly the IsStandingStill function is called on relevant events
        - worldPlacedItem is now reset to empty table instead of nil to prevent crash when calling tableHelper.isEqualTo with nil value
        - OnObjectDialogueChoice uses validator instead of handler as a callback, newly the companion's inventory is force loaded
          upon player selecting 'Companion Share' dialogue to prevent observed desync connected with other player modifying the contents
          behind the scenes

--- 0.8.1/companionShare/main.lua
+++ 0.8.1/companionShare/main.lua
@@ -12,6 +12,19 @@
 |   3. Open customScripts.lua and add there the following line: require("custom.companionShare.main")                                    |
 |   4. Save customScripts.lua and launch the server                                                                                      |
 |   5. To confirm that the script is running, you should see "[CompanionShare] Running..." among the first few lines of server console   |
+|                                                                                                                                        |
+| changelog:                                                                                                                             |
+|   1.1 - removed timer for checking player standing still, newly the IsStandingStill function is called on relevant events              |
+|       - worldPlacedItem is now reset to empty table instead of nil to prevent crash when calling tableHelper.isEqualTo with nil value  |
+|       - OnObjectDialogueChoice uses validator instead of handler as a callback, newly the companion's inventory is force loaded        |
+|         upon player selecting 'Companion Share' dialogue to prevent observed desync connected with other player modifying the contents |
+|         behind the scenes                                                                                                              |
+|   1.0 - initial version                                                                                                                |
+|                                                                                                                                        |
+| known limitations:                                                                                                                     |
+|   1. - if player closes 'Companion Share', yet does not move and other player modifies the inventory of the same companion,            |
+|        the companion will be reactivated for the player and 'Companion Share' opened, this is due to me not knowing of a way to        |
+|        determine, whether the player has closed the inventory or not -> this can be avoided by slightly moving/turning upon closure    |
 ==========================================================================================================================================
 ]]
 
@@ -25,7 +38,7 @@ script.IsPlayerLoggedIn = function(pid)
     return Players[pid] ~= nil and Players[pid]:IsLoggedIn()
 end
 
--- Returns true if player's position/rotation has not changed since activation of Companion Share, false otherwise
+-- Returns true if player's position/rotation has not changed since activation of Companion Share, false otherwise.
 script.IsStandingStill = function(pid)
     local lastPosX = Players[pid].companionShare.lastKnownPosition.posX
     local lastPosY = Players[pid].companionShare.lastKnownPosition.posY
@@ -42,28 +55,19 @@ script.IsStandingStill = function(pid)
     return true
 end
 
--- Timer that gets executed periodically to detect player leaving the Companion Share
-companion_share_exit_check_timer = function(pid)
-    if not script.IsPlayerLoggedIn(pid) then return end
+-- Validator that force loads the inventory of the companion for the player, it seems that if there are any changes done to the companion inventory while player does not have it open
+-- the inventory is not synced upon opening without the force load.
+-- Tracks player's position/rotation and companion uniqueIndex upon activating Companion Share.
+-- Lastly it also creates a helper variable to detect an item that has been removed from player's inventory, yet dropped into the world instead of into the companion's inventory.
+script.OnObjectDialogueChoiceValidator = function(eventStatus, pid, cellDescription, objects)
+    local cell = LoadedCells[cellDescription]
 
-    if not script.IsStandingStill(pid) then
-        Players[pid].companionShare = nil
-        return
-    end
-
-    tes3mp.RestartTimer(Players[pid].companionShare.exitCheckTimer, 100)
-end
-
--- Handler that tracks player's position/rotation and companion uniqueIndex upon activating Companion Share.
--- It also creates and starts a timer used for checking whether the player has moved - that is a necessary workaround since MenuMode does not return 1 in tes3mp.
--- Lastly it also creates a helper variable to detect an item that has been removed from player's inventory, yet dropped into the world instead of into the companion's inventory
-script.OnObjectDialogueChoiceHandler = function(eventStatus, pid, cellDescription, objects)
     for uniqueIndex, object in pairs(objects) do
-        if object.dialogueChoiceType == enumerations.dialogueChoice.COMPANION_SHARE then
+        if object.dialogueChoiceType == enumerations.dialogueChoice.COMPANION_SHARE and cell ~= nil and cell:ContainsObject(uniqueIndex) then
             if script.IsPlayerLoggedIn(pid) then
+                cell:LoadContainers(pid, cell.data.objectData, {uniqueIndex})
                 Players[pid].companionShare = {
                     companionIndex = uniqueIndex,
-                    exitCheckTimer = tes3mp.CreateTimerEx("companion_share_exit_check_timer", 1000, "i", pid),
                     lastKnownPosition = {
                         posX = tes3mp.GetPosX(pid),
                         posY = tes3mp.GetPosY(pid),
@@ -73,14 +77,12 @@ script.OnObjectDialogueChoiceHandler = function(eventStatus, pid, cellDescriptio
                     },
                     worldPlacedItem = {}
                 }
-                tes3mp.StartTimer(Players[pid].companionShare.exitCheckTimer)
             end
         end
     end
 end
 
--- Handler that tracks an item dropped by the player into the world.
--- That is because OnObjectPlace event is fired earlier than OnPlayerInventory.
+-- Handler that tracks an item dropped by the player into the world (OnObjectPlace event is fired earlier than OnPlayerInventory).
 script.OnObjectPlaceHandler = function(eventStatus, pid, cellDescription, objects)
     for uniqueIndex, object in pairs(objects) do
         if object.droppedByPlayer then
@@ -92,9 +94,9 @@ script.OnObjectPlaceHandler = function(eventStatus, pid, cellDescription, object
 end
 
 -- Handler that:
--- a) checks whether the item the packet is about has not been dropped into the world by player in the previous OnObjectPlace event - if so, it nullifies the worldPlacedItem and returns
+-- a) checks whether the item the packet is about has not been dropped into the world by player in the previous OnObjectPlace event - if so, it empties the worldPlacedItem and returns
 -- b) adds an item to companion's inventory and updates the UI accordingly for other players that may have the Companion Share activated for the same uniqueIndex
--- c) removes an item (or some of it) from the companion's inventory and updates the UI accordingly for other players that may have the Companion Share activated for the same uniqueIndex
+-- c) removes an item (or part of it) from the companion's inventory and updates the UI accordingly for other players that may have the Companion Share activated for the same uniqueIndex
 script.OnPlayerInventoryHandler = function(eventStatus, pid, playerPacket)
     if not script.IsPlayerLoggedIn(pid) then return end
 
@@ -103,8 +105,13 @@ script.OnPlayerInventoryHandler = function(eventStatus, pid, playerPacket)
     local item = playerPacket.inventory[1]
 
     if Players[pid].companionShare ~= nil and cell ~= nil then
+        if not script.IsStandingStill(pid) then
+            Players[pid].companionShare = nil
+            return
+        end
+
         if tableHelper.isEqualTo(item, Players[pid].companionShare.worldPlacedItem) then
-            Players[pid].companionShare.worldPlacedItem = nil
+            Players[pid].companionShare.worldPlacedItem = {}
             return
         end
 
@@ -119,46 +126,51 @@ script.OnPlayerInventoryHandler = function(eventStatus, pid, playerPacket)
 
         if playerPacket.action == enumerations.inventory.ADD then
             inventoryHelper.removeExactItem(companionInventory, item.refId, item.count, item.charge, item.enchantmentCharge, item.soul)
-            script.UpdateContainerUi(pid, cellDescription, companionIndex, cell.data.objectData[companionIndex].refId)
+            script.UpdateContainerUiForOthers(pid, cellDescription, companionIndex, cell.data.objectData[companionIndex].refId)
         elseif playerPacket.action == enumerations.inventory.REMOVE then
             inventoryHelper.addItem(companionInventory, item.refId, item.count, item.charge, item.enchantmentCharge, item.soul)
-            script.UpdateContainerUi(pid, cellDescription, companionIndex, cell.data.objectData[companionIndex].refId)
+            script.UpdateContainerUiForOthers(pid, cellDescription, companionIndex, cell.data.objectData[companionIndex].refId)
         end
     end
 end
 
--- A workaround to update the UI of the companion's inventory for other players that may have it opened.
--- It executes the 'ToggleMenus' command twice for the other player (who has the companion's inventory opened), first one to hide the UI, second one to refresh it.
--- Then it loads the inventory of the companion that has been updated via the OnPlayerInventory handler.
--- Last but not least it activates the companion for the player again and lastly it selects the Companion Share for them - this should be seamless, player should only notice the inventory of the companion
+-- A workaround to update the UI of the companion's inventory for other players that may have it opened,
+-- it executes the 'ToggleMenus' command twice for the other player (who has the companion's inventory opened), first one to hide the UI, second one to refresh it,
+-- then it force loads the inventory of the companion that has been updated via the OnPlayerInventory handler for the other player.
+-- Last but not least it activates the companion for the other player again and lastly it selects the Companion Share for them - this should be seamless, other player should only notice the inventory of the companion
 -- being updated, no other artifacts.
-script.UpdateContainerUi = function(pid, cellDescription, companionIndex, refId)
+script.UpdateContainerUiForOthers = function(pid, cellDescription, companionIndex, refId)
     local cell = LoadedCells[cellDescription]
 
     for opid, _ in pairs(Players) do
         if opid ~= pid and script.IsPlayerLoggedIn(opid) and Players[opid].companionShare ~= nil and Players[opid].companionShare.companionIndex == companionIndex and cell ~= nil then
-            logicHandler.RunConsoleCommandOnPlayer(opid, "ToggleMenus", false)
-            logicHandler.RunConsoleCommandOnPlayer(opid, "ToggleMenus", false)
-            cell:LoadContainers(opid, cell.data.objectData, {companionIndex})
-            logicHandler.ActivateObjectForPlayer(opid, cellDescription, companionIndex)
+            if script.IsStandingStill(opid) then
+                logicHandler.RunConsoleCommandOnPlayer(opid, "ToggleMenus", false)
+                logicHandler.RunConsoleCommandOnPlayer(opid, "ToggleMenus", false)
+                cell:LoadContainers(opid, cell.data.objectData, {companionIndex})
+                logicHandler.ActivateObjectForPlayer(opid, cellDescription, companionIndex)
 
-            tes3mp.ClearObjectList()
-            tes3mp.SetObjectListPid(opid)
-            tes3mp.SetObjectListCell(cellDescription)
+                tes3mp.ClearObjectList()
+                tes3mp.SetObjectListPid(opid)
+                tes3mp.SetObjectListCell(cellDescription)
 
-            local splitIndex = companionIndex:split("-")
-            tes3mp.SetObjectRefNum(splitIndex[1])
-            tes3mp.SetObjectMpNum(splitIndex[2])
-            tes3mp.SetObjectRefId(refId)
-            tes3mp.SetObjectDialogueChoiceType(enumerations.dialogueChoice.COMPANION_SHARE)
-            tes3mp.AddObject()
+                local splitIndex = companionIndex:split("-")
+                tes3mp.SetObjectRefNum(splitIndex[1])
+                tes3mp.SetObjectMpNum(splitIndex[2])
 
-            tes3mp.SendObjectDialogueChoice(false, false)
+                tes3mp.SetObjectRefId(refId)
+                tes3mp.SetObjectDialogueChoiceType(enumerations.dialogueChoice.COMPANION_SHARE)
+                tes3mp.AddObject()
+
+                tes3mp.SendObjectDialogueChoice(false, false)
+            else
+                Players[opid].companionShare = nil
+            end
         end
     end
 end
 
 customEventHooks.registerHandler("OnServerPostInit", script.OnServerPostInitHandler)
-customEventHooks.registerHandler("OnObjectDialogueChoice", script.OnObjectDialogueChoiceHandler)
+customEventHooks.registerHandler("OnObjectDialogueChoice", script.OnObjectDialogueChoiceValidator)
 customEventHooks.registerHandler("OnObjectPlace", script.OnObjectPlaceHandler)
 customEventHooks.registerHandler("OnPlayerInventory", script.OnPlayerInventoryHandler)
